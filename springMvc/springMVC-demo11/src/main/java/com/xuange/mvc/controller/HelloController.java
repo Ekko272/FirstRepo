36bca3c5643f715281cbcd65a99f796eb7b24131
Ekko272 <122116946+Ekko272@users.noreply.github.com>
2023-07-17 22:36:28 -0700

    Add files via upload

---
+++ springMvc/springMVC-demo11/src/main/java/com/xuange/mvc/controller/HelloController.java
@@ -0,0 +1,11 @@
+package com.xuange.mvc.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
